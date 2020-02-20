3f49c8b58ebfe253edd1668f5b61beb7dbbf79a0
SergeiMeza <sergeimeza@gmail.com>
2020-02-20 14:35:49 +0900

    Initial Commit

---
+++ demo-client-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo-client-ios
+//
+//  Created by Meza Rodriguez Jeany Sergei on 2020/02/20.
+//  Copyright © 2020 C Channel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
